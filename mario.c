6137547cb4cbae7acceaf1fa8dc52033401c08fa
sujay2019 <ovbs.lavanya@gmail.com>
2019-07-30 21:42:54 -0700

    Mario.c is a program to add numbers.

---
+++ mario.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <cs50.h>
+int main()
+{
+    int a,b,c;
+printf("enter the first value\n");
+scanf("%d",&a);
+printf("enter the second value\n");
+    scanf("%d",&b);
+    
+printf ("%d + %d = %d\n", a, b, c);
+}
+
