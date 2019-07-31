2e5e05faa00b458bba5d44f591f0e0dcdfdafa45
sujay2019 <ovbs.lavanya@gmail.com>
2019-07-31 16:51:01 -0700

    This is a test for users.

---
+++ test.c
@@ -0,0 +1,15 @@
+##include <stdio.h>
+#include <cs50.h>
+
+int main (){
+    int a=54367;
+    if (a>10){
+        printf("The equation is true \n");
+    }
+  else
+  
+      printf("The equation is false");
+  
+    
+    }
+    
