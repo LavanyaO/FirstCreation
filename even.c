b3caf9fc21daaf7b543d26994158062c44fd5340
sujay2019 <ovbs.lavanya@gmail.com>
2019-07-31 17:07:58 -0700

    This is a very fun script, do not take anything seriously and report on us.

---
+++ even.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main()  {
+    int a=18;
+    if (a%2==0){
+  printf("The given number is even ,go play a lot of video games, you are very smart, go do software engineering!!");
+       
+    }
+ else 
+     printf("The given number is not even, you are very bad,go learn some math and do not play video games! Shame on you Go work at taco bell!");
+}
+
