45a01d40a24b1032bb4496f55b9679ad02f1bdec
sujay2019 <ovbs.lavanya@gmail.com>
2019-08-01 20:12:31 -0700

    this is a gamefile

---
+++ game.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+int main () {
+printf ("You need to defeat the dragon, type in a even number to win.");
+int a=422;
+if (a%2==0)
+printf ("You have sucsessfully killed the dragon,nice job ,you are now the king of the kingdom");
+else 
+
+printf("You have lost shame on you now you will work for the kingdom !");
+       }
+
